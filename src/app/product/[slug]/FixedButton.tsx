26d018efd5ef41b402407521ee69c179e77e51a3
Alva Jufinto <yeahbruh199@gmail.com>
2024-09-06 17:21:14 +0700

    rm unused package and add phoneNumber to contact

--- src/app/product/[slug]/FixedButton.tsx
+++ src/app/product/[slug]/FixedButton.tsx
@@ -8,6 +8,15 @@ import Image from "next/image";
 import WAIcon from "@/assets/icons/wa-icon-white.svg";
 
 export default function FixedButton() {
+  const phoneNumber = process.env.NEXT_PUBLIC_PHONE_NUMBER;
+
+  const routeToWhatsApp = () => {
+    if (!phoneNumber) return;
+
+    const text = `https://wa.me/${phoneNumber}`;
+    window.open(text, "_blank");
+  };
+
   useEffect(() => {
     // Get the button and footer elements, cast them to HTMLElement
     const button = document.querySelector(
@@ -16,10 +25,7 @@ export default function FixedButton() {
     const footer = document.getElementById("footer") as HTMLElement | null;
 
     // If either element is missing, exit early
-    if (!button || !footer) {
-      console.warn("Button or Footer element not found");
-      return;
-    }
+    if (!button || !footer) return;
 
     // Callback function for Intersection Observer
     const handleIntersect = (entries: IntersectionObserverEntry[]) => {
@@ -51,7 +57,9 @@ export default function FixedButton() {
 
   return (
     <div className="mt-[100px] button-container relative">
-      <button className="fixed-button fixed bottom-[40px] right-[50%] translate-x-[50%] items-center justify-center rounded-[72px] h-[72px] w-[312px] font-bold text-n-100 bg-[#0EA46D] text-[1.25rem] gap-3 hidden sm:flex">
+      <button
+        className="fixed-button fixed bottom-[40px] right-[50%] translate-x-[50%] items-center justify-center rounded-[72px] h-[72px] w-[312px] font-bold text-n-100 bg-[#0EA46D] text-[1.25rem] gap-3 hidden sm:flex"
+        onClick={routeToWhatsApp}>
         <Image src={WAIcon} alt="wa-icon" /> Contact Via Whatsapp
       </button>
     </div>
